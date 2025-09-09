65c54a06647a7a4047712d90c2baaafb4758e105
ivex0002 <garam199803@gmail.com>
2025-09-09 15:29:16 +0900

    ✨ Feat:FE_014:커서 위치 감지, 스토어에 저장
    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import './App.css'
-import Background from './components/Background'
+import { useEffect } from "react";
+import "./App.css";
+import Background from "./components/Background";
+import { setMousePosition } from "./hooks/useMousePosition";
 
 function App() {
 
-  return (
-    <Background>
-    </Background>
-  )
+  // mouse position set
+  const updateMousePosition = (e: MouseEvent) => {
+    setMousePosition(e.clientX, e.clientY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("mousemove", updateMousePosition);
+
+    return () => {
+      window.removeEventListener("mousemove", updateMousePosition);
+    };
+  }, []);
+
+  return <Background></Background>;
 }
 
-export default App
+export default App;
