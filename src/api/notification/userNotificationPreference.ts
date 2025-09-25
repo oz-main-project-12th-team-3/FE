9b08bd1eff984b0bcd7ceaa7fba0ac5c4600ccdd
ivex0002 <garam199803@gmail.com>
2025-09-25 14:35:40 +0900

    api 콜 헬퍼 수정 및 알림 api 코드 리팩토링

--- src/api/notification/userNotificationPreference.ts
+++ src/api/notification/userNotificationPreference.ts
@@ -1,53 +1,12 @@
-import { api } from "../baseApi";
+import { handleApiCall } from "../apiCallHelper";
 
-type NotificationPreference = {
+export type NotificationPreference = {
   id: number;
   notification_type_id: number;
   is_enabled: boolean;
   created_at: string;
   updated_at: string;
 };
-
-export interface GetUsersNotificationPreferencesApiRes {
-  notificationPreference: NotificationPreference[];
-}
-
-/**
- * 사용자 알림 설정 목록들을 조회하는 GET메서드
- * @returns {Promise<GetUsersNotificationPreferencesApiRes>} 응답 데이터
- */
-export async function getUsersNotificationPreferencesApi(): Promise<GetUsersNotificationPreferencesApiRes> {
-  try {
-    const res = await api.get(`/users/notification-preferences`);
-    return res.data;
-  } catch (error: any) {
-    const message = error.response?.data?.message || error.message;
-    throw new Error(`getUsersNotificationPreferencesApi failed: ${message}`);
-  }
-}
-
-export interface GetSingleUserNotificationPreferenceApiRes {
-  notificationPreference: NotificationPreference;
-}
-
-/**
- * 사용자의 특정 알림 설정 상세 조회하는 GET메서드
- * @param {number} id 특정 알림 설정 항목의 id
- * @returns {Promise<GetSingleUserNotificationPreferenceApiRes>} 응답 데이터
- */
-export async function getSingleUserNotificationPreferenceApi(
-  id: number
-): Promise<GetSingleUserNotificationPreferenceApiRes> {
-  try {
-    const res = await api.get(`/users/notification-preferences/${id}`);
-    return res.data;
-  } catch (error: any) {
-    const message = error.response?.data?.message || error.message;
-    throw new Error(
-      `getSingleUserNotificationPreferenceApi failed: ${message}`
-    );
-  }
-}
 
 export interface PostNotificationPreferenceApiReq {
   notification_type_id: number;
@@ -62,23 +21,6 @@ export interface PostNotificationPreferenceApiRes {
   updated_at: string;
 }
 
-/**
- * 사용자 알림 설정 생성하는 POST메서드
- * @param {PostNotificationPreferenceApiReq} payload - 요청 데이터
- * @returns {Promise<PostNotificationPreferenceApiRes>} 응답 데이터
- */
-export async function postNotificationPreferencesApi(
-  payload: PostNotificationPreferenceApiReq
-): Promise<PostNotificationPreferenceApiRes> {
-  try {
-    const res = await api.post(`/users/notification-preferences`, payload);
-    return res.data;
-  } catch (error: any) {
-    const message = error.response?.data?.message || error.message;
-    throw new Error(`postNotificationPreferecesApi failed: ${message}`);
-  }
-}
-
 export interface PutNotificationPreferenceApiReq {
   is_enabled: boolean;
 }
@@ -90,42 +32,65 @@ export interface PutNotificationPreferenceApiRes {
   updated_at: string;
 }
 
-/**
- * 사용자 알림 설정을 수정하는 PUT메서드
- * @param {number} id 변경할 알림 설정의 id
- * @param {PutNotificationPreferenceApiReq} payload - 요청 데이터
- * @returns {Promise<PutNotificationPreferenceApiRes>} 응답 데이터
- */
-export async function putNotificationPreferenceApi(
-  id: number,
-  payload: PutNotificationPreferenceApiReq
-): Promise<PutNotificationPreferenceApiRes> {
-  try {
-    const res = await api.put(`/users/notification-preferences/${id}`, payload);
-    return res.data;
-  } catch (error: any) {
-    const message = error.response?.data?.message || error.message;
-    throw new Error(`putNotificationPreferenceApi failed: ${message}`);
-  }
-}
-
 export interface DeleteUserNotificationPreferenceApiRes {
   detail: string;
 }
 
-/**
- * 사용자 알림 설정 삭제하는 DELETE메서드 
- * @param {number} id 삭제할 유저알림설정의 id
- * @returns {Promise<DeleteUserNotificationPreferenceApiRes>} 응답 데이터
- */
-export async function deleteUserNotificationPreferenceApi(
-  id: number
-): Promise<DeleteUserNotificationPreferenceApiRes> {
-  try {
-    const res = await api.delete(`/users/notification-preferences/${id}`);
-    return res.data;
-  } catch (error: any) {
-    const message = error.response?.data?.message || error.message;
-    throw new Error(`deleteUserNotificationPreferenceApi failed: ${message}`);
-  }
-}
+export const userNotificationPreferenceApi = {
+  GET: {
+    /**
+     * 사용자 알림 설정 목록들을 조회하는 GET메서드
+     * @returns {Promise<NotificationPreference[]>} 응답 데이터
+     */
+    userNotiPrefs: async () => {
+      const url = `/users/notification-preferences`;
+      return await handleApiCall({ method: "GET", url: url });
+    },
+    /**
+     * 사용자의 특정 알림 설정 상세 조회하는 GET메서드
+     * @param {number} id 특정 알림 설정 항목의 id
+     * @returns {Promise<NotificationPreference>} 응답 데이터
+     */
+    userNotiPrefById: async (id: number) => {
+      const url = `/users/notification-preferences/${id}`;
+      return await handleApiCall({ method: "GET", url: url });
+    },
+  },
+  POST: {
+    /**
+     * 사용자 알림 설정 생성하는 POST메서드
+     * @param {PostNotificationPreferenceApiReq} payload - 요청 데이터
+     * @returns {Promise<PostNotificationPreferenceApiRes>} 응답 데이터
+     */
+    notiPref: async (payload: PostNotificationPreferenceApiReq) => {
+      const url = `/users/notification-preferences`;
+      return await handleApiCall({ method: "POST", url: url, data: payload });
+    },
+  },
+  PUT: {
+    /**
+     * 사용자 알림 설정을 수정하는 PUT메서드
+     * @param {number} id 변경할 알림 설정의 id
+     * @param {PutNotificationPreferenceApiReq} payload - 요청 데이터
+     * @returns {Promise<PutNotificationPreferenceApiRes>} 응답 데이터
+     */
+    notiPrefById: async (
+      id: number,
+      payload: PutNotificationPreferenceApiReq
+    ) => {
+      const url = `/users/notification-preferences/${id}`;
+      return await handleApiCall({ method: "PUT", url: url, data: payload });
+    },
+  },
+  DELETE: {
+    /**
+     * 사용자 알림 설정 삭제하는 DELETE메서드
+     * @param {number} id 삭제할 유저알림설정의 id
+     * @returns {Promise<DeleteUserNotificationPreferenceApiRes>} 응답 데이터
+     */
+    notiPrefById: async (id: number) => {
+      const url = `/users/notification-preferences/${id}`;
+      return await handleApiCall({ method: "DELETE", url: url });
+    },
+  },
+};
