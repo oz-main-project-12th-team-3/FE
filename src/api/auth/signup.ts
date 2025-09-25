10fac461afee1be97bd7d98e61ba8e638c08c67b
ivex0002 <garam199803@gmail.com>
2025-09-25 14:50:09 +0900

    auth api  리팩토링

--- src/api/auth/signup.ts
+++ src/api/auth/signup.ts
@@ -1,12 +1,12 @@
-import { api } from "../baseApi";
+import { handleApiCall } from "../apiCallHelper";
 
-interface SignupReq {
+export interface SignupReq {
   email: string;
   password: string;
   nickname: string;
 }
 
-interface SignupRes {
+export interface SignupRes {
   id: number;
   email: string;
   role: string;
@@ -16,17 +16,16 @@ interface SignupRes {
   detail: string;
 }
 
-/**
- *
- * @param {SignupReq} payload  이메일, 비번, 닉네임
- * @returns {SignupRes} .detail에 "회원가입이 성공적으로 완료되었습니다." 반환됨
- */
-export async function signupApi(payload: SignupReq): Promise<SignupRes> {
-  try {
-    const res = await api.post("/auth/signup", payload);
-    return res.data;
-  } catch (error: any) {
-    const message = error.response?.data?.message || error.message;
-    throw new Error(`signupApi failed: ${message}`);
-  }
-}
+export const signupApi = {
+  POST: {
+    /**
+     * 회원가입 메서드
+     * @param {SignupReq} payload  이메일, 비번, 닉네임
+     * @returns {Promise<SignupRes>} .detail에 "회원가입이 성공적으로 완료되었습니다." 반환됨
+     */
+    signup: async (payload:SignupReq) => {
+      const url = `/auth/signup`;
+      return await handleApiCall({ method: "POST", url: url, data:payload });
+    },
+  },
+};
