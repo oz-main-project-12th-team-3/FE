0fab1731e9442c4a31450fa919ba8b2c8df1b5ff
ivex0002 <garam199803@gmail.com>
2025-09-26 14:40:35 +0900

    로그인 로직, 토큰관리 수정 및 로그인시 엑세스 토큰 반드시 응답 바디로 받도록 명시

--- src/api/auth/login.ts
+++ src/api/auth/login.ts
@@ -1,15 +1,29 @@
-import { handleApiCall } from "../apiClient";
+import { handleApiCall, TokenManager } from "../apiClient";
 
-export interface LoginReq {
+interface LoginReq {
   email: string;
   password: string;
 }
 
-export interface LoginRes {
+interface LoginRes {
   detail: string;
   user_id: number;
+  email:string
   expires_in: number;
+  access_token: string;
+  tfa_required: boolean;
+  tfa_step:string
+  temporary_access_token:string
+  temporary_refresh_token:string
 }
+
+type EmailCheckReq = {
+  email: string;
+};
+type EmailCheckRes = {
+  detail: string;
+  available: boolean;
+};
 
 export const loginApi = {
   POST: {
@@ -18,8 +32,29 @@ export const loginApi = {
      * @param {LoginReq} payload 이메일, 비번
      * @returns {Promise<LoginRes>} .detail에 "로그인 성공" 반환됨
      */
-    login: async (payload: LoginReq) => {
-      const url = `/auth/login/`;
+    login: async (payload: LoginReq): Promise<LoginRes> => {
+      const url = `/api/v1/auth/login/`;
+      const res = await handleApiCall<LoginRes>({
+        method: "POST",
+        url: url,
+        data: payload,
+      });
+      TokenManager.setTokens(
+        res.access_token,
+        res.user_id.toString(),
+        res.expires_in
+      );
+      return res;
+    },
+    /**
+     * 이메일 중복 체크
+     * @param {EmailCheckReq} payload 중복 체크할 이메일
+     * @returns {Promise<EmailCheckRes>} .detail에 응답 메시지 .available에 불린값
+     */
+    checkEmailDuplicate: async (
+      payload: EmailCheckReq
+    ): Promise<EmailCheckRes> => {
+      const url = `/api/v1/auth/email-check/`;
       return await handleApiCall({ method: "POST", url: url, data: payload });
     },
   },
