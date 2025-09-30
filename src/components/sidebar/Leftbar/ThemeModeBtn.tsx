b6cf26724fd4452a726d69e8c6b499692467aece
ivex0002 <garam199803@gmail.com>
2025-09-30 17:33:33 +0900

    토글 버튼 분리

--- src/components/sidebar/Leftbar/ThemeModeBtn.tsx
+++ src/components/sidebar/Leftbar/ThemeModeBtn.tsx
@@ -1,57 +1,9 @@
-/** @jsxImportSource @emotion/react */
-import { motion } from "framer-motion";
-import { themeKeys } from "../../../styles/constColors";
 import { storeColorMode } from "../../../store/storeColorMode";
-import { useThemeColors } from "../../../hooks/useThemeColors";
-import { css } from "@emotion/react";
+import { themeKeys } from "../../../styles/constColors";
+import { ToggleBtn } from "../../utilComponents/ToggleBtn";
 
 export function ThemeModeBtn() {
   const { mode, toggle } = storeColorMode();
-  const { text, background } = useThemeColors();
 
-  const colorCss = css`
-    border: 1px solid ${text};
-    background-color: ${background};
-    .circle {
-      background-color: ${text};
-    }
-  `;
-
-  return (
-    <motion.div
-      css={[DarkWhiteModeBtnCss, colorCss]}
-      onClick={toggle}
-      whileTap={{ scale: 0.95 }}
-    >
-      <motion.div
-        className="circle"
-        layout
-        animate={{
-          x: mode === themeKeys[0] ? 16 : -15,
-        }}
-        transition={{
-          type: "spring",
-          stiffness: 500,
-          damping: 30,
-        }}
-      />
-    </motion.div>
-  );
+  return <ToggleBtn bool={mode === themeKeys[0]} toggle={toggle} />;
 }
-
-const DarkWhiteModeBtnCss = css`
-  --btn-height: 2rem;
-  width: calc(var(--btn-height) * 2);
-  height: var(--btn-height);
-  border-radius: 1.5rem;
-  position: relative;
-
-  .circle {
-    height: calc(var(--btn-height) * 0.9);
-    aspect-ratio: 1 / 1;
-    border-radius: 50%;
-    position: absolute;
-    top: 1px;
-    left:calc(var(--btn-height) * 0.5);
-  }
-`;
