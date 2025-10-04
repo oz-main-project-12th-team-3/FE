6943ee59f1421932e543d0f40a60f682c135588f
ivex0002 <garam199803@gmail.com>
2025-10-04 15:01:02 +0900

    백그라운드 구조 개선

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,32 @@ import WindowSizeProvider from "./components/utilComponents/WindowSizeProvider";
 import { MousePositonSetter } from "./components/utilComponents/MousePositionSetter";
 import { ToastContainer } from "react-toastify";
 
-
 // Home은 즉시 로드
 import Home from "./components/Home";
+import { ThemeModeBtn } from "./components/sidebar/Leftbar/ThemeModeBtn";
 
 const Modal = React.lazy(() => import("./components/modal/Modal"));
-const AuthModal = React.lazy(() => import("./components/modal/authmodal/AuthModal"));
-const ScheduleModal = React.lazy(() => import("./components/modal/schedule_modal/scheduleModal"));
-const NotificationModal = React.lazy(() => import("./components/modal/notification_modal/NotificationModal"))
-const TermsModal = React.lazy(() => import("./components/modal/authmodal/TermsModal"));
-const PasswordChangeModal = React.lazy(() => import("./components/modal/passwordChang/PasswordChangeModal"))
-const DeleteAccountModal = React.lazy(() => import("./components/modal/deleteAccount_modal/DeleteAccountModal"))
-const ProfileSettingModal = React.lazy(() => import("./components/modal/profileSetting_modal/ProfileSettingModal"))
-
+const AuthModal = React.lazy(
+  () => import("./components/modal/authmodal/AuthModal")
+);
+const ScheduleModal = React.lazy(
+  () => import("./components/modal/schedule_modal/scheduleModal")
+);
+const NotificationModal = React.lazy(
+  () => import("./components/modal/notification_modal/NotificationModal")
+);
+const TermsModal = React.lazy(
+  () => import("./components/modal/authmodal/TermsModal")
+);
+const PasswordChangeModal = React.lazy(
+  () => import("./components/modal/passwordChang/PasswordChangeModal")
+);
+const DeleteAccountModal = React.lazy(
+  () => import("./components/modal/deleteAccount_modal/DeleteAccountModal")
+);
+const ProfileSettingModal = React.lazy(
+  () => import("./components/modal/profileSetting_modal/ProfileSettingModal")
+);
 
 export default function AppDesktop() {
   useEffect(() => {
@@ -27,40 +40,40 @@ export default function AppDesktop() {
 
   return (
     <>
-        <Background>
-          <WindowSizeProvider />
-          <MousePositonSetter />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/chat/:session_id" element={<Home />} />
-            <Route path="/chat/new" element={<Home />} />
+      <Background>
+        <WindowSizeProvider />
+        <MousePositonSetter />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/chat/:session_id" element={<Home />} />
+          <Route path="/chat/new" element={<Home />} />
 
-              <Route path="modal" element={<Modal />}>
-                <Route path="auth" element={<AuthModal />} />
-                <Route path="terms" element={<TermsModal />} />
-                <Route path="schedule" element={<ScheduleModal />} />
-                <Route path="notification" element={<NotificationModal />} />
-                <Route path="password" element={<PasswordChangeModal />} />
-                <Route path="deleteaccount" element={<DeleteAccountModal />} />
-                <Route path="profilesetting" element={<ProfileSettingModal/>} />
-              </Route>
-            </Routes>
-        </Background>
+          <Route path="modal" element={<Modal />}>
+            <Route path="auth" element={<AuthModal />} />
+            <Route path="terms" element={<TermsModal />} />
+            <Route path="schedule" element={<ScheduleModal />} />
+            <Route path="notification" element={<NotificationModal />} />
+            <Route path="password" element={<PasswordChangeModal />} />
+            <Route path="deleteaccount" element={<DeleteAccountModal />} />
+            <Route path="profilesetting" element={<ProfileSettingModal />} />
+          </Route>
+        </Routes>
+      </Background>
+      <ThemeModeBtn/>
 
-        <ToastContainer
-              position="top-center"  // 위치
-              autoClose={3000}       // 3초 후 닫힘
-              hideProgressBar        // 프로그레스바 표시 여부
-              newestOnTop
-              closeOnClick
-              rtl={false}
-              pauseOnFocusLoss
-              draggable
-              pauseOnHover
-              theme="light"
-              style={{ zIndex: 9999 }} // 모달보다 높게 지정
-            />
-    
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        style={{ zIndex: 9999 }}
+      />
     </>
   );
 }
--- src/components/sidebar/Leftbar/ThemeModeBtn.tsx
+++ src/components/sidebar/Leftbar/ThemeModeBtn.tsx
@@ -1,43 +1,43 @@
 /** @jsxImportSource @emotion/react */
-// import { css } from "@emotion/react";
+import { motion } from "framer-motion";
 import { storeColorMode } from "../../../store/storeColorMode";
 import { themeKeys } from "../../../styles/constColors";
 import { ToggleBtn } from "../../utilComponents/ToggleBtn";
-
-// export function ThemeModeBtn() {
-//   const { mode, toggle } = storeColorMode();
-//   const [bool, setBool] = useState(mode === themeKeys[0]);
-
-//   useEffect(() => {
-//   setBool(mode === themeKeys[0]);
-// }, [mode]);
-
-//   const handleToggle = () => {
-//     setBool((v) => !v);
-//     toggle();
-//   };
-
-//   return <ToggleBtn bool={bool} toggle={handleToggle} />;
-// }
+import {
+  SIDEBAR_MARGIN,
+  SIDEBAR_WIDTH,
+  useMousePositionStore,
+} from "../../../store/useMousePositionStore";
+import { css } from "@emotion/react";
 
 export function ThemeModeBtn() {
   const { mode, toggle } = storeColorMode();
+  const { whereIsMouse } = useMousePositionStore();
 
-  return <ToggleBtn bool={mode === themeKeys[0]} toggle={toggle} />;
+  return (
+    <motion.div
+      initial={{
+        x:
+          whereIsMouse === "left"
+            ? SIDEBAR_MARGIN
+            : -SIDEBAR_WIDTH - SIDEBAR_MARGIN,
+      }}
+      animate={{
+        x:
+          whereIsMouse === "left"
+            ? SIDEBAR_MARGIN
+            : -SIDEBAR_WIDTH - SIDEBAR_MARGIN,
+      }}
+      transition={{ type: "spring", stiffness: 300, damping: 30 }}
+    >
+      <div css={positionCss} className="mode_btn">
+        <ToggleBtn bool={mode === themeKeys[0]} toggle={toggle} />
+      </div>
+    </motion.div>
+  );
 }
 
-// const positionCss = css`
-//   position: absolute;
-//   top:1rem;
-//   right: 1rem;
-// `
+const positionCss = css`
+  position: absolute;
 
-// export function ThemeModeBtn() {
-//   const { isDark, toggle } = storeColorMode();
-
-//   return (
-//     <div css={positionCss}>
-//       <ToggleBtn bool={isDark} toggle={toggle} />
-//     </div>
-//   );
-// }
+`
