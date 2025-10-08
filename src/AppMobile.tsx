d1293baabbe168aeafe7238d62b067c0e26f14c5
ivex0002 <garam199803@gmail.com>
2025-10-08 16:32:50 +0900

    앱 모바일 단순화, 배경 임포팅 제거

--- src/AppMobile.tsx
+++ src/AppMobile.tsx
@@ -1,25 +1,8 @@
-import React from "react";
 import "./App.css";
-import ContentRerender from "./components/background/Background";
-import { Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
-
-const Modal = React.lazy(() => import("./components/modal/Modal"));
-const Login = React.lazy(() => import("./components/modal/authmodal/LoginForm"));
 
 // 단순 분기 처리만 한 상태
 // 반드시 앱 데스크탑 파일을 참조하여 작성할것!!
 
 export default function AppMobile() {
-  return (
-    <ContentRerender>
-      <Routes>
-        <Route path="/" element={<Home />} />
-
-        <Route path="modal" element={<Modal />}>
-          <Route path="login" element={<Login />} />
-        </Route>
-      </Routes>
-    </ContentRerender>
-  );
+  return <>모바일 화면 아직 작업중입니다....</>;
 }
