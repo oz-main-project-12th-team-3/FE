6b7f091feb09a32d2b6462154de4290fd414f254
ivex0002 <garam199803@gmail.com>
2025-10-10 17:41:49 +0900

    del:voice api >음성채팅 전부 수정해야됨

--- src/api/voice/voiceLog.ts
+++ src/api/voice/voiceLog.ts
@@ -1,69 +1,72 @@
-import { handleApiCall } from "../apiClient";
+// import { handleApiCall } from "../apiClient";
 
-type VoiceLog = {
-  id: number;
-  user_id: number;
-  session_id: number;
-  input_audio_url: string;
-  output_audio_url: string | null;
-  transcribed_text: string | null;
-  timestamp: string;
-  created_at: string;
-  updated_at: string;
-};
+// type VoiceLog = {
+//   id: number;
+//   user_id: number;
+//   session_id: number;
+//   input_audio_url: string;
+//   output_audio_url: string | null;
+//   transcribed_text: string | null;
+//   timestamp: string;
+//   created_at: string;
+//   updated_at: string;
+// };
 
-interface PostVoiceLogApiReq {
-  user_id: number;
-  session_id: number;
-  input_audio_url: string;
-}
+// interface PostVoiceLogApiReq {
+//   user_id: number;
+//   session_id: number;
+//   input_audio_url: string;
+// }
 
-type DeleteVoiceLogRes = {
-  detail: string;
-};
+// type DeleteVoiceLogRes = {
+//   detail: string;
+// };
 
-export const voiceLogApi = {
-  GET: {
-    /**
-     * 음성 로그 조회 (세션별)
-     * @param {number} sessionId 세션 id
-     * @returns {Promise<VoiceLog[]>} voiceLog[]
-     */
-    voiceLogsBySessionId: async (sessionId: number): Promise<VoiceLog[]> => {
-      const url = `/voice-logs?session_id=${sessionId}/`;
-      return await handleApiCall({ method: "GET", url: url });
-    },
-    /**
-     * 단일 음성 로그 조회
-     * @param {number} id 조회할 음성 로그 id
-     * @returns {Promise<VoiceLog>} voiceLog 타입
-     */
-    voiceLogById: async (id: number): Promise<VoiceLog> => {
-      const url = `/voice-logs/${id}/`;
-      return await handleApiCall({ method: "GET", url: url });
-    },
-  },
-  POST: {
-    /**
-     * 음성채팅 생성 메서드
-     * @param {PostVoiceLogApiReq} payload 유저id, 세션id, 인풋url
-     * @returns {Promise<VoiceLog>} voiceLog 타입
-     */
-    voiceLog: async (payload: PostVoiceLogApiReq): Promise<VoiceLog> => {
-      const url = `/voice-logs/`;
-      return await handleApiCall({ method: "POST", url: url, data: payload });
-    },
-  },
-  PUT: {},
-  DELETE: {
-    /**
-     * 음성 로그 삭제
-     * @param {number} id 삭제할 로그의 id
-     * @returns {Promise<DeleteVoiceLogRes>}
-     */
-    voiceLogById: async (id: number): Promise<DeleteVoiceLogRes> => {
-      const url = `/voice-logs/${id}/`;
-      return await handleApiCall({ method: "DELETE", url: url });
-    },
-  },
-};
+// export const voiceLogApi = {
+//   GET: {
+//     /**
+//      * 음성 로그 조회 (세션별)
+//      * @param {number} sessionId 세션 id
+//      * @returns {Promise<VoiceLog[]>} voiceLog[]
+//      */
+//     voiceLogsBySessionId: async (sessionId: number): Promise<VoiceLog[]> => {
+//       const url = `/voice-logs?session_id=${sessionId}/`;
+//       return await handleApiCall({ method: "GET", url: url });
+//     },
+//     /**
+//      * 단일 음성 로그 조회
+//      * @param {number} id 조회할 음성 로그 id
+//      * @returns {Promise<VoiceLog>} voiceLog 타입
+//      */
+//     voiceLogById: async (id: number): Promise<VoiceLog> => {
+//       const url = `/voice-logs/${id}/`;
+//       return await handleApiCall({ method: "GET", url: url });
+//     },
+//   },
+//   POST: {
+//     /**
+//      * 음성채팅 생성 메서드
+//      * @param {PostVoiceLogApiReq} payload 유저id, 세션id, 인풋url
+//      * @returns {Promise<VoiceLog>} voiceLog 타입
+//      */
+//     voiceLog: async (payload: PostVoiceLogApiReq): Promise<VoiceLog> => {
+//       const url = `/voice-logs/`;
+//       return await handleApiCall({ method: "POST", url: url, data: payload });
+//     },
+//   },
+//   PUT: {},
+//   DELETE: {
+//     /**
+//      * 음성 로그 삭제
+//      * @param {number} id 삭제할 로그의 id
+//      * @returns {Promise<DeleteVoiceLogRes>}
+//      */
+//     voiceLogById: async (id: number): Promise<DeleteVoiceLogRes> => {
+//       const url = `/voice-logs/${id}/`;
+//       return await handleApiCall({ method: "DELETE", url: url });
+//     },
+//   },
+// };
+
+// voice 파트 완전히 사라짐
+// 왜 아무말도 안해주지....
