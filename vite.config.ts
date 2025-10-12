fea81f4bcbd58ca8f20257971c1175344105d299
BE12-OZ <dhwmzheld6470@gmail.com>
2025-10-13 04:11:29 +0900

    SCRUM-18: Frontend: Fix Emotion CSS prop TypeScript error in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,13 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: ['@emotion/babel-plugin'],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       'src': path.resolve(__dirname, './src'),
